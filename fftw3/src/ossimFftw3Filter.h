44e218ac298011fdcb9c32b155fc084fdca80ddb
oscarkramer <okramer@radiantblue.com>
2016-07-18 13:57:54 -0400

    Refactored FFTW3 to derive from ossimFftFilter since much in common

--- fftw3/src/ossimFftw3Filter.h
+++ fftw3/src/ossimFftw3Filter.h
@@ -7,48 +7,19 @@
 
 #ifndef ossimFftw3Filter_HEADER
 #define ossimFftw3Filter_HEADER
-#include <ossim/imaging/ossimImageSourceFilter.h>
+#include <ossim/imaging/ossimFftFilter.h>
 
-class ossimScalarRemapper;
-
-class ossimFftw3Filter : public ossimImageSourceFilter
+class ossimFftw3Filter : public ossimFftFilter
 {
 public:
-   enum TransformDirection  { FORWARD = -1, INVERSE=1 };
-
    ossimFftw3Filter(ossimObject* owner=NULL);
    ossimFftw3Filter(ossimImageSource* inputSource);
-   ossimFftw3Filter(ossimObject* owner,
-                  ossimImageSource* inputSource);
-   virtual ossimRefPtr<ossimImageData> getTile(const ossimIrect& rect, ossim_uint32 resLevel=0);
-   virtual void initialize();
-   virtual ossim_uint32 getNumberOfOutputBands() const;
-   virtual double getMinPixelValue(ossim_uint32 band=0)const;
-   virtual double getMaxPixelValue(ossim_uint32 band=0)const;
-   virtual double getNullPixelValue(ossim_uint32 band=0)const;
-   virtual ossimScalarType getOutputScalarType() const;
+   ossimFftw3Filter(ossimObject* owner, ossimImageSource* inputSource);
    
-   void setForward();
-   void setInverse();
-   ossimString getDirectionTypeAsString()const;
-   void setDirectionType(const ossimString& directionType);
-   void setDirectionType(TransformDirection directionType);
-      
-   virtual ossimRefPtr<ossimProperty> getProperty(const ossimString& name)const;
-   virtual void setProperty(ossimRefPtr<ossimProperty> property);
-   virtual void getPropertyNames(std::vector<ossimString>& propertyNames)const;
-
-   virtual bool loadState(const ossimKeywordlist& kwl,
-                          const char* prefix = 0);
-   virtual bool saveState(ossimKeywordlist& kwl,
-                          const char* prefix = 0)const;
 protected:
-   void runFft(ossimRefPtr<ossimImageData>& input, ossimRefPtr<ossimImageData>& output);
+   virtual void runFft(ossimRefPtr<ossimImageData>& input, ossimRefPtr<ossimImageData>& output);
 
    virtual ~ossimFftw3Filter();
-   ossimRefPtr<ossimImageData> m_tile;
-   TransformDirection m_xformDir;
-   ossimRefPtr<ossimScalarRemapper> m_scalarRemapper;
 
 TYPE_DATA
 };
