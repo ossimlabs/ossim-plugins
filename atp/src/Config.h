72e2d2b703978ded0ed81be189d8117b43c6e941
oscarkramer <okramer@radiantblue.com>
2018-03-15 19:19:49 -0400

    Removed unused header

--- atp/src/Config.h
+++
@@ -1,7 +0,0 @@
-//**************************************************************************************************
-//
-//     OSSIM Open Source Geospatial Data Processing Library
-//     See top level LICENSE.txt file for license information
-//
-//**************************************************************************************************
-#define OSSIM_HAS_OPENCV_HIGHGUI 1
